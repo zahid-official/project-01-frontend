cddb81f12a6b5fe05005df7e95e4075980b1e8da
zahid-official <zahid.official8@gmail.com>
2025-09-24 11:24:30 +0600

    chore: initialize project with shadcn/ui

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return (
+    <>
+      <h1>hello world</h1>
+      <Button>Click me</Button>
+    </>
+  );
+};
+
+export default App;
