f3834ecc871fca329e353801d69b00ad17f42a2d
zahid-official <zahid.official8@gmail.com>
2025-09-25 23:03:39 +0600

    Update readme

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./global.css";
 import { RouterProvider } from "react-router";
-import Router from "./routes/routes";
+import Router from "./routes";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
