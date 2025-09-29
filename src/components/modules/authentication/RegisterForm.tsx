587479149c7138a82fd6333bb8925e314d405cbe
zahid-official <zahid.official8@gmail.com>
2025-09-29 22:25:44 +0600

    feat(auth): intregrate react-hook-form and zod for form validation in register form

--- src/components/modules/authentication/RegisterForm.tsx
+++ src/components/modules/authentication/RegisterForm.tsx
@@ -1,15 +1,100 @@
+import { z } from "zod";
 import { cn } from "@/lib/utils";
 import { Link } from "react-router";
-import { Button } from "@/components/ui/button";
+import { useForm } from "react-hook-form";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
+import { zodResolver } from "@hookform/resolvers/zod";
+import InputPassword from "@/components/ui/input-password";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
+// Zod schema
+const registerZodSchema = z
+  .object({
+    // Name
+    name: z
+      .string()
+      .min(2, { error: "Name must be at least 2 characters long." })
+      .max(50, { error: "Name cannot exceed 50 characters." })
+      .trim(),
+
+    // Email
+    email: z
+      .email()
+      .min(5, { error: "Email must be at least 5 characters long." })
+      .max(100, { error: "Email cannot exceed 100 characters." })
+      .trim(),
+
+    // Password
+    password: z
+      .string()
+      .min(8, { error: "Password must be at least 8 characters long." })
+
+      // Password complexity requirements
+      .regex(/^(?=.*[A-Z])/, {
+        error: "Password must contain at least 1 uppercase letter.",
+      })
+      .regex(/^(?=.*[!@#$%^&*])/, {
+        error: "Password must contain at least 1 special character.",
+      })
+      .regex(/^(?=.*\d)/, {
+        error: "Password must contain at least 1 number.",
+      })
+      .trim(),
+
+    // Confirm password
+    confirmPassword: z
+      .string()
+      .min(8, { error: "Confirm Password must be at least 8 characters long." })
+
+      // Password complexity requirements
+      .regex(/^(?=.*[A-Z])/, {
+        error: "Confirm Password must contain at least 1 uppercase letter.",
+      })
+      .regex(/^(?=.*[!@#$%^&*])/, {
+        error: "Confirm Password must contain at least 1 special character.",
+      })
+      .regex(/^(?=.*\d)/, {
+        error: "Confirm Password must contain at least 1 number.",
+      })
+      .trim(),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    error: "Password's don't match",
+    path: ["confirmPassword"],
+  });
 
 const RegisterForm = ({
   className,
   ...props
-}: React.ComponentProps<"form">) => {
+}: React.HTMLAttributes<HTMLDivElement>) => {
+  // useForm hook
+  const form = useForm<z.infer<typeof registerZodSchema>>({
+    resolver: zodResolver(registerZodSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
+
+  // Handle onSubmit
+  const onSubmit = (data: z.infer<typeof registerZodSchema>) => {
+    console.log(data);
+  };
+
   return (
-    <form className={cn("flex flex-col gap-6", className)} {...props}>
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      {/* Heading */}
       <div className="flex flex-col items-center gap-2 text-center">
         <h1 className="text-2xl font-bold">Register new account</h1>
         <p className="text-muted-foreground text-sm text-balance">
@@ -17,26 +102,90 @@ const RegisterForm = ({
         </p>
       </div>
 
+      {/* Form body */}
       <div className="grid gap-6">
-        {/* Email */}
-        <div className="grid gap-3">
-          <Label htmlFor="email">Email</Label>
-          <Input id="email" type="email" placeholder="m@example.com" required />
-        </div>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+            {/* Name */}
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input placeholder="John Doe" {...field} />
+                  </FormControl>
+                  <FormDescription className="sr-only">
+                    This is your public display name.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
 
-        {/* Password */}
-        <div className="grid gap-3">
-          <div className="flex items-center">
-            <Label htmlFor="password">Password</Label>
-          </div>
-          <Input id="password" type="password" required />
-        </div>
+            {/* Email */}
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input placeholder="john@email.com" {...field} />
+                  </FormControl>
+                  <FormDescription className="sr-only">
+                    This is your public display email.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
 
-        {/* Submin btn */}
-        <Button type="submit" className="w-full">
-          Register
-        </Button>
+            {/* Password */}
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <InputPassword {...field} />
+                  </FormControl>
+                  <FormDescription className="sr-only">
+                    This is your account password.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
 
+            {/* Confirm password */}
+            <FormField
+              control={form.control}
+              name="confirmPassword"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Confirm Password</FormLabel>
+                  <FormControl>
+                    <InputPassword {...field} />
+                  </FormControl>
+                  <FormDescription className="sr-only">
+                    This is your confirm password.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            {/* Submit btn */}
+            <Button type="submit" className="w-full cursor-pointer">
+              Submit
+            </Button>
+          </form>
+        </Form>
+
+        {/* Divider */}
         <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
           <span className="bg-background text-muted-foreground relative z-10 px-2">
             Or continue with
@@ -44,7 +193,7 @@ const RegisterForm = ({
         </div>
 
         {/* Google register */}
-        <Button variant="outline" className="w-full">
+        <Button variant="outline" className="w-full cursor-pointer">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 262">
             <path
               fill="currentColor"
@@ -62,7 +211,7 @@ const RegisterForm = ({
           Login
         </Link>
       </div>
-    </form>
+    </div>
   );
 };
 
